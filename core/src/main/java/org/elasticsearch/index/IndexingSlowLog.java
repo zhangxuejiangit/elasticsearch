7c55ce3d5f4c46a433e3f0d6dc95b6216e9b825a
张雪键 <zhangxuejian@outlook.com>
2020-06-20 14:34:10 +0800

    add unique_id field and update overflow logic

--- core/src/main/java/org/elasticsearch/index/IndexingSlowLog.java
+++ core/src/main/java/org/elasticsearch/index/IndexingSlowLog.java
@@ -189,7 +189,9 @@ public final class IndexingSlowLog implements IndexingOperationListener {
             sb.append("type[").append(doc.type()).append("], ");
             sb.append("id[").append(doc.id()).append("], ");
             sb.append("version[").append(doc.version().stringValue()).append("], ");
-
+            sb.append("unique_id[").append(index.getName()).append(":").append(doc.type()).append(":")
+                .append(doc.id()).append(":").append(doc.version().stringValue()).append("], ");
+            
             if (doc.routing() == null) {
                 sb.append("routing[] ");
             } else {
@@ -199,9 +201,15 @@ public final class IndexingSlowLog implements IndexingOperationListener {
             if (maxSourceCharsToLog == 0 || doc.source() == null || doc.source().length() == 0) {
                 return sb.toString();
             }
+
             try {
                 String source = XContentHelper.convertToJson(doc.source(), reformat, doc.getXContentType());
-                sb.append(", source[").append(Strings.cleanTruncate(source, maxSourceCharsToLog)).append("]");
+                //sb.append(", source[").append(Strings.cleanTruncate(source, maxSourceCharsToLog)).append("]");
+                if (maxSourceCharsToLog < source.length()) {
+                    sb.append(", source[_source_overflow_]");
+                } else {
+                    sb.append(", source[").append(source).append("]");
+                }
             } catch (IOException e) {
                 sb.append(", source[_failed_to_convert_]");
             }
